dcd9bcfac32312a62c139da783466c16fc9c7b15
Anton Goroshko <radjahair@gmail.com>
2023-08-13 18:07:17 +1000

    Add new classes

---
+++ station.rb
@@ -0,0 +1,20 @@
+class Station
+  attr_reader :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def accept_train(train)
+    trains << train
+  end
+
+  def dispatch_train(train)
+    trains.delete(train)
+  end
+
+  def trains_by_type(type)
+    trains.filter { |train| train.type == type }
+  end
+end
