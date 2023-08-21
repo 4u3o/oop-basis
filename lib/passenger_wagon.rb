eef709e3d2d795c53005486a3b140c974452199c
Anton Goroshko <radjahair@gmail.com>
2023-08-21 19:47:35 +1000

    Add seats

--- lib/passenger_wagon.rb
+++ lib/passenger_wagon.rb
@@ -1,5 +1,17 @@
 class PassengerWagon < Wagon
-  def initialize
+  attr_reader :seats_taken
+
+  def initialize(seats_total)
     @type = :passenger
+    @seats_total = seats_total
+    @seats_taken = 0
+  end
+
+  def seats_free
+    seats_total - seats_taken
+  end
+
+  def take_seat
+    self.seats_taken += 1
   end
 end
